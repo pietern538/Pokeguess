b9cd563786110786189ff6d1c91ec4ddb0b8ca34
pietern538 <pietern3@gmail.com>
2026-03-02 16:49:56 +0100

    Update pokemon_guess_game_mc.py

--- pokemon_guess_game_mc.py
+++ pokemon_guess_game_mc.py
@@ -18,7 +18,7 @@ def get_valid_cards():
         SELECT c.id, c.name, c.card_number, c.set_name, c.image, p.lowest_near_mint
         FROM cards c
         JOIN prices p ON c.id = p.card_id
-        WHERE c.image IS NOT NULL AND p.lowest_near_mint IS NOT NULL AND p.lowest_near_mint > 2
+        WHERE c.image IS NOT NULL AND p.lowest_near_mint IS NOT NULL AND p.lowest_near_mint > 2 AND c.supertype = 'Pokémon'
     """)
     cards = cursor.fetchall()
     conn.close()
@@ -98,3 +98,4 @@ if cards:
     st.write(f"Score: {st.session_state.score} / {st.session_state.rounds * 9 if st.session_state.rounds else 1}")
 else:
     st.warning("Not enough valid cards found in the database.")
+
